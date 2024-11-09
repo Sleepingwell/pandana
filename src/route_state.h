74c08fcb6e56817f38a5d8ded88aa4baa8ba3868
Simon Knapp <simon.knapp@data61.csiro.au>
2024-11-09 13:06:45 +1100

    Save full results for each link in simulations.
    Enables showing histograms.

--- src/route_state.h
+++ src/route_state.h
@@ -1,6 +1,7 @@
 #ifndef PANDANA_ROUTE_STATE_HEADER_INCLUDED_A0897G0AOIU
 #define PANDANA_ROUTE_STATE_HEADER_INCLUDED_A0897G0AOIU
 
+#include <filesystem>
 #include <map>
 #include <string>
 #include <vector>
@@ -10,16 +11,19 @@ namespace MTC::accessibility {
 
 class RoutingStatsState {
 public:
-    using StatsVector = std::vector<double>;
-    explicit RoutingStatsState(int);
+    using StatsVector = std::vector<float>;
+    explicit RoutingStatsState(int n_links) : n_links_(n_links) {}
     StatsVector& operator[](std::string const&);
-    void serialise(const char*, int);
-    int max_link_id() const { return max_link_id_; }
+    void serialise(const std::filesystem::path& output_dir, int);
+    int n_links() const { return n_links_; }
 
 private:
     std::map<std::string, StatsVector> stats_;
-    int max_link_id_;
+    int n_links_;
 };
+
+void do_transpose(const char* filename, int n_simulations, int n_links);
+std::vector<float> do_extract_rows(const char* filename, std::vector<int> const& link_ids, int n_simulations);
 
 } // end MTC::accessibility
 
